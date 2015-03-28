a2aa3fe230ab1d69980506f6191437162a5a30a2
Ruy Garcia <rrgarciach@gmail.com>
2015-03-27 20:26:23 -0600

    Trying to optimise dynamic memory vs signal arrays.

--- IRSignalSender.cpp
+++ IRSignalSender.cpp
@@ -5,7 +5,7 @@ IRSignalSender::IRSignalSender(int pin)
 {
 	pinMode(pin,OUTPUT);
 	digitalWrite(pin, LOW);
-	_NumIRsignals = 100;
+	// _NumIRsignals = 100;
 	_IRledPin = pin;
 }
 
@@ -20,17 +20,17 @@ void IRSignalSender::sendCommand(int command)
 void IRSignalSender::parsePulse(int command) {
 	switch (command) {
 		case 0:
-			_NumIRsignals = cmd_Midea_OnOff[0];
+			_NumIRsignals = cmd_on[0];
 			for (int i = 1; i < _NumIRsignals; i += 2) {
-				pulseIR(cmd_Midea_OnOff[i]*10);
-				delayMicroseconds(cmd_Midea_OnOff[i+1]*10);
+				pulseIR(cmd_on[i]*10);
+				delayMicroseconds(cmd_on[i+1]*10);
 			}
 		break;
 		case 1:
-			_NumIRsignals = cmd_SamsungTV_OnOff[0];
+			_NumIRsignals = cmd_off[0];
 			for (int i = 1; i < _NumIRsignals; i += 2) {
-				pulseIR(cmd_SamsungTV_OnOff[i]*10);
-				delayMicroseconds(cmd_SamsungTV_OnOff[i+1]*10);
+				pulseIR(cmd_off[i]*10);
+				delayMicroseconds(cmd_off[i+1]*10);
 			}
 		break;
 	}
