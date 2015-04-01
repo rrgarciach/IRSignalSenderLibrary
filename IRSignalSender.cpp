c0ab230c2422e424932806c49de4e9493224d139
Ruy Garcia <rrgarciach@gmail.com>
2015-04-01 00:31:37 -0600

    Enhanced saving a lot of memory just by diving the pulses value by 2.

--- IRSignalSender.cpp
+++ IRSignalSender.cpp
@@ -20,18 +20,18 @@ void IRSignalSender::parsePulse(uint8_t command) {
 	switch (command) {
 		// TURN ON command:
 		case 0:
-			_NumIRsignals = cmd_on[0];
+			_NumIRsignals = cmd_on[0]*2;
 			for (int i = 1; i < _NumIRsignals; i += 2) {
-				pulseIR(cmd_on[i]*10);
-				delayMicroseconds(cmd_on[i+1]*10);
+				pulseIR(cmd_on[i]*10*2);
+				delayMicroseconds(cmd_on[i+1]*10*2);
 			}
 		break;
 		// TURN OFF command:
 		case 1:
-			_NumIRsignals = cmd_off[0];
+			_NumIRsignals = cmd_off[0]*2;
 			for (int i = 1; i < _NumIRsignals; i += 2) {
-				pulseIR(cmd_off[i]*10);
-				delayMicroseconds(cmd_off[i+1]*10);
+				pulseIR(cmd_off[i]*10*2);
+				delayMicroseconds(cmd_off[i+1]*10*2);
 			}
 		break;
 	}
